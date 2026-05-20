874bdaf416ad2df2499e6bd176a910af2823f70d
nofuruu2 <nfatihul7@gmail.com>
2026-05-20 19:53:22 +0700

    make sure this application is static without any databases used in here

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -2,13 +2,13 @@ import { redirect } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "./$types.js";
 
 export const load: LayoutServerLoad = async ({ locals }) => {
-if (!locals.user) {
-redirect(302, "/login");
-}
+	if (!locals.user) {
+		redirect(302, "/login");
+	}
 
-return {
-user: locals.user,
-unreadNotificationCount: 0,
-recentNotifications: [],
-};
+	return {
+		user: locals.user,
+		unreadNotificationCount: 0,
+		recentNotifications: [],
+	};
 };
